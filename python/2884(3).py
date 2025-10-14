4e01d535417e85f8fed7f39c14ff1d4827edf084
재덕 <145331405+jaeduck223@users.noreply.github.com>
2025-10-14 16:02:07 +0900

    Delete python directory

--- python/2884(3).py
+++
@@ -1,9 +0,0 @@
-h, m = map(int, input().split())
-
-if ((m - 45) < 0) and ((h - 1) >= 0):
-    print(h - 1, m - 45 + 60)
-elif ((m - 45) < 0) and ((h - 1) < 0):
-    print(h + 23, m - 45 + 60)
-else:
-    print(h, m - 45)
-    
